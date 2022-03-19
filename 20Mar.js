5514c0a6d95a3da2ca9d0bfc3b713f046f5dd035
Vi <viminehan@gmail.com>
2022-03-20 09:47:47 +1100

    Refactor 20Mar

--- 20Mar.js
+++ 20Mar.js
@@ -18,3 +18,17 @@ function booleanToString(b){
     } else { 
     return 'Draw!'}
   }
+
+  const rps = (p1, p2) => {
+      if (p1 == p2) {
+          return 'Draw!';
+      }else if (p1 == 'rock' && p2 == 'scissors') {
+          return 'Player 1 won!';
+      } else if (p1 == 'paper' && p2 == 'rock') {
+          return 'Player 1 won!';
+      } else if (p1 == 'scissors' && p2 == 'paper') {
+          return 'Player 1 won!';
+      } else {
+          return 'Player 2 won!';
+      }
+  }
