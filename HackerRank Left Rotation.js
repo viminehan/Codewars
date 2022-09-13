94523f6e6e435a7a85286a86590eddec75b1631b
Vi <viminehan@gmail.com>
2022-09-13 17:13:39 +1000

    Create HackerRank Left Rotation.js

---
+++ HackerRank Left Rotation.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'rotateLeft' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER d
+ *  2. INTEGER_ARRAY arr
+ */
+
+function rotateLeft(d, arr) {
+    // Write your code here
+    let result = arr;
+    for (let i = 0; i < d; i++) { 
+     let hold = arr[0];
+     result.shift(arr[0]);
+     result.push(hold);   
+    }
+    
+    return result;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const d = parseInt(firstMultipleInput[1], 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = rotateLeft(d, arr);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
